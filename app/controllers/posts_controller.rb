71ab1e3350d6b0cf7738daeb735129b07f2ef422
Wilner Charles <wilnercharles3@gmail.com>
2017-12-06 14:00:08 -0500

    views and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,40 @@
 class PostsController < ApplicationController
-def index
-  @post = Post.all
-end
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+    def index
+    @posts = Post.all
+    end
+    def update
+    if  @post.update
+    redirect_to @post
+    else
+    render 'edit'
+    end
+    def edit
+    end
+    def destroy
+    @post.destroy
+    redirect_to root_path
+    end
+    def show
+    end
+    def new
+    @post = Post.new
+    end
+    def create
+    @post = Post.create!(post_params)
+    if @post.save
+    redirect_to @post
+    else
+    render 'new'
+    end
+   end
+
+   private
+
+   def find_post
+     @post = Post.find(params[:id])
+   end
+    def post_params
+     params.require(:post).permit(:title, :content)
+   end
 end
